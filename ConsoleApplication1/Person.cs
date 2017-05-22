af18c91dc389f1ebb5f0aaf9d822e7d947364168
jiaokang <Administrator@HP201-1>
2017-05-22 15:52:46 +0800

    构造函数

---
+++ ConsoleApplication1/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Person
+    {
+        private int age;//字段
+        private string name;
+        public int Age
+        {
+            get { return age; }// 取值
+            set { age = value; }// 赋值
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Person()  //构造方法,初始化变量
+        {
+            Console.WriteLine("人类的构造函数");
+        }
+        public Person(string name,int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,15 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+
+            Person p = new Person();
+            //p.Name = "zhangsan";
+            //p.Age = 21;
+            string strname = p.Name;
+            int strage = p.Age;
+
+            Person p2 = new Person("zhangsan", 21);
+           
         }
     }
 }
